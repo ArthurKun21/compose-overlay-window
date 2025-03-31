286d45ba56075a327cfd94df434dc36412c43bcb
arthur <16458204+ArthurKun021@users.noreply.github.com>
2025-03-31 14:46:49 +0800

    fix: update import path for resource references in DialogPermission

--- app/src/main/java/com/github/only52607/compose/window/ui/DialogPermission.kt
+++ app/src/main/java/com/github/only52607/compose/window/ui/DialogPermission.kt
@@ -18,7 +18,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import com.github.only52607.compose.window.R
+import com.github.only52607.compose.window.app.R
 
 @Composable
 fun DialogPermission(
