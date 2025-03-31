be2dc72bd420b60de691df170d7391cf579278be
arthur <16458204+ArthurKun021@users.noreply.github.com>
2025-03-31 16:15:37 +0800

    feat: integrate state management for button enablement

--- app/src/main/java/com/github/only52607/compose/window/app/MainActivity.kt
+++ app/src/main/java/com/github/only52607/compose/window/app/MainActivity.kt
@@ -12,11 +12,13 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.github.only52607.compose.window.ComposeFloatingWindow
 import com.github.only52607.compose.window.app.ui.DialogPermission
 import com.github.only52607.compose.window.app.ui.FloatingWindowContent
@@ -33,6 +35,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeFloatingWindowTheme {
                 val showDialogPermission = remember { mutableStateOf(false) }
+
+                val showing by floatingWindow.showing.collectAsStateWithLifecycle()
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
@@ -42,19 +47,25 @@ class MainActivity : ComponentActivity() {
                         verticalArrangement = Arrangement.Center,
                         horizontalAlignment = Alignment.CenterHorizontally
                     ) {
-                        Button(onClick = {
-                            if (floatingWindow.isAvailable()) {
-                                show()
-                            } else {
-                                showDialogPermission.value = true
-                            }
-                        }) {
+                        Button(
+                            onClick = {
+                                if (floatingWindow.isAvailable()) {
+                                    show()
+                                } else {
+                                    showDialogPermission.value = true
+                                }
+                            },
+                            enabled = !showing
+                        ) {
                             Text("Show")
                         }
                         Spacer(modifier = Modifier.height(10.dp))
-                        Button(onClick = {
-                            hide()
-                        }) {
+                        Button(
+                            onClick = {
+                                hide()
+                            },
+                            enabled = showing
+                        ) {
                             Text("Hide")
                         }
                     }
