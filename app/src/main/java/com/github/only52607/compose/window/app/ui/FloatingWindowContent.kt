bbd9d0e2f2f75b616d90159141cf530669839218
arthur <16458204+ArthurKun021@users.noreply.github.com>
2025-04-01 10:29:17 +0800

    feat: add elevation to sample fab to remove shadow

--- app/src/main/java/com/github/only52607/compose/window/app/ui/FloatingWindowContent.kt
+++ app/src/main/java/com/github/only52607/compose/window/app/ui/FloatingWindowContent.kt
@@ -3,12 +3,14 @@ package com.github.only52607.compose.window.app.ui
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Call
 import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.SystemAlertDialog
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.github.only52607.compose.window.LocalFloatingWindow
 import com.github.only52607.compose.window.dragFloatingWindow
@@ -35,7 +37,11 @@ fun FloatingWindowContent(
         modifier = Modifier.dragFloatingWindow(),
         onClick = {
             model.showDialog()
-        }) {
+        },
+        elevation = FloatingActionButtonDefaults.elevation(
+            defaultElevation = 0.dp
+        )
+    ) {
         Icon(Icons.Filled.Call, "Call")
     }
 }
