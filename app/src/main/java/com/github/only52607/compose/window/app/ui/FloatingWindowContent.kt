e8564e6fcd6612410273a87bde6d565fe3c17b57
arthur <16458204+ArthurKun021@users.noreply.github.com>
2025-05-05 10:05:05 +0800

    chore: organize imports in FloatingWindowContent.kt

--- app/src/main/java/com/github/only52607/compose/window/app/ui/FloatingWindowContent.kt
+++ app/src/main/java/com/github/only52607/compose/window/app/ui/FloatingWindowContent.kt
@@ -1,5 +1,6 @@
 package com.github.only52607.compose.window.app.ui
 
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Call
 import androidx.compose.material3.FloatingActionButton
@@ -9,8 +10,10 @@ import androidx.compose.material3.SystemAlertDialog
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
