c5a81f7431671daae0d8fb70a3cd2c1661187236
arthur <16458204+ArthurKun021@users.noreply.github.com>
2025-08-21 21:21:15 +0800

    fix: update package name in instrumented test for fullscreen dialog

--- samples/fullscreen-dialog/src/androidTest/java/io/github/arthurkun/fullscreen/dialog/ExampleInstrumentedTest.kt
+++ samples/fullscreen-dialog/src/androidTest/java/io/github/arthurkun/fullscreen/dialog/ExampleInstrumentedTest.kt
@@ -17,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("io.github.arthurkun.app.activity", appContext.packageName)
+        assertEquals("io.github.arthurkun.fullscreen.dialog", appContext.packageName)
     }
 }
