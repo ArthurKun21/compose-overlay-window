e7d2c4d66aafce55f65d729771a28abcbb7fea40
Arthur <16458204+ArthurKun21@users.noreply.github.com>
2025-08-22 06:37:35 +0800

    feat: add support for keyboard usage with floating window functionality

---
+++ samples/keyboard-usage/src/main/java/io/github/arthurkun/keyboard/usage/ui/floating/FloatingScreen.kt
@@ -0,0 +1,56 @@
+package io.github.arthurkun.keyboard.usage.ui.floating
+
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.SystemAlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.github.only52607.compose.window.dragFloatingWindow
+import io.github.arthurkun.keyboard.usage.ui.theme.ComposeFloatingWindowTheme
+
+@Composable
+fun FloatingScreen(
+    vm: FloatingViewModel = viewModel(),
+) {
+    val showing by vm.dialogVisible.collectAsStateWithLifecycle()
+
+    ComposeFloatingWindowTheme {
+        if (showing) {
+            SystemAlertDialog(
+                onDismissRequest = { vm.dismissDialog() },
+                confirmButton = {
+                    TextButton(onClick = { vm.dismissDialog() }) {
+                        Text(text = "OK")
+                    }
+                },
+                text = {
+                    Text(text = "This is a system dialog")
+                },
+            )
+        }
+
+        FloatingActionButton(
+            modifier = Modifier.dragFloatingWindow(),
+            onClick = {
+                vm.showDialog()
+            },
+        ) {
+            AnimatedContent(showing) { isVisible ->
+                if (isVisible) {
+                    Icon(Icons.Default.Close, contentDescription = "Hide Dialog")
+                } else {
+                    Icon(Icons.Default.Done, contentDescription = "Show Dialog")
+                }
+            }
+        }
+    }
+}
