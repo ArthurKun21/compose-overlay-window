d1b812d5429ee93b8e57d818a749bc43e8183efc
Arthur <16458204+ArthurKun21@users.noreply.github.com>
2025-08-21 17:02:43 +0800

    refactor: enhance Gradle configuration by specifying included groups for Google repositories

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,31 @@
 pluginManagement {
     repositories {
-        google()
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
         mavenCentral()
         gradlePluginPortal()
+
     }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        google()
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        // fallback for the rest of the dependencies
         mavenCentral()
-        maven("https://jitpack.io")
+        maven { url = uri("https://jitpack.io") }
+
     }
 }
 
